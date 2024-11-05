063da9a97978422eb27a7e4d1ef8b50eec0757af
AgustinMeneses <agustinmeneses15@gmail.com>
2024-11-05 01:26:12 -0300

    Initial commit

---
+++ Scripts/Big Button.gd
@@ -0,0 +1,33 @@
+extends StaticBody3D
+class_name size_button
+
+@onready var anim = $AnimationPlayer
+
+@export var max_size:float=4
+@export var min_size:float=0.2
+@export var size_speed:int=3
+@export var agrandar:bool
+
+var player: CharacterBody3D
+
+func _process(delta):
+	if agrandar:
+		if player==null or player.scale.x>=max_size: return
+		player.scale.x+=delta*size_speed
+		player.scale.y+=delta*size_speed
+		player.scale.z+=delta*size_speed
+	else:
+		if player==null or player.scale.x<=min_size: return
+		player.scale.x-=delta*size_speed
+		player.scale.y-=delta*size_speed
+		player.scale.z-=delta*size_speed
+	pass
+	
+func _on_effect_area_body_entered(body):
+	player=body
+	pass # Replace with function body.
+
+
+func _on_effect_area_body_exited(body):
+	player=null
+	pass # Replace with function body.
---
+++ Scripts/restart_button.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+func _on_body_entered(body:CharacterBody3D):
+	body.scale.x=1
+	body.scale.y=1
+	body.scale.z=1
+	pass # Replace with function body.
