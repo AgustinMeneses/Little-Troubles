3e8470f2ef55ca5b245798e3683332313e227de5
Mister 5 <agustinmeneses15@gmail.com>
2024-11-11 14:26:07 -0300

    Delete Little Troubles directory

--- Little Troubles/Scripts/Object 1.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody3D
-class_name Pickable
-
-var is_pickable:bool=false
-@export var masa:float
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	add_to_group("obejtos")
-	pass # Replace with function body.
