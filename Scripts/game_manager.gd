6846b74b98a5c0f9ad602216d5611ca6320ec4a1
Mister 5 <agustinmeneses15@gmail.com>
2024-12-09 19:35:13 -0300

    Delete Scripts directory

--- Scripts/game_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var levels_completeds : Array[String] = []
-
-func level_completed(level : String):
-	levels_completeds.append(level)
--- Scripts/restart_button.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody3D
-
-func _on_area_3d_body_entered(body):
-	body.fuerza=5
-	body.scale.x=1
-	body.scale.y=1
-	body.scale.z=1
-	pass # Replace with function body.
